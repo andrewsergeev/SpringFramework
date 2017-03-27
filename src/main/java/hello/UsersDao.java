5da5d938c62db47f6fc2cf8a1d1413f59bee3b3e
Andrew Sergeev <aelsergeev@gmail.com>
2017-03-27 08:27:11 +0300

    add dao

---
+++ src/main/java/hello/UsersDao.java
@@ -0,0 +1,1 @@
+package hello;import org.springframework.data.repository.CrudRepository;import javax.transaction.Transactional;@Transactionalpublic interface UsersDao extends CrudRepository<Users, Long> {    Users findByEmail(String email);}
