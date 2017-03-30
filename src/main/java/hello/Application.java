62e666963c5d8f604530c6c4fd7c072e63f5683f
aelsergeev <aelsergeev@gmail.com>
2017-03-30 15:17:31 +0300

    add hibernate

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,26 +1,12 @@
 package hello;
 
 import com.google.gson.Gson;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileInputStream;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
 
 
 @SpringBootApplication
